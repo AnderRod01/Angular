79a94958053308fef709f26c0a53e35dd596da9d
AnderRod01 <80098555+AnderRod01@users.noreply.github.com>
2023-01-25 12:52:18 +0100

    /

---
+++ appTablaJSON/src/app/components/database-tabla/database-tabla.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-database-tabla',
+  templateUrl: './database-tabla.component.html',
+  styles: [
+  ]
+})
+export class DatabaseTablaComponent {
+
+}
